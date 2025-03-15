9adb4f1d758e678c315b7bc002d8ac02ffd18757
Atharv Dalal <sangamdalal24@gmail.com>
2025-03-15 21:43:26 +0530

    Booking Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ServiceRequest } from './enitities/services/service-request-enitiy';
 import { AuthModule } from './auth/auth.module';
 import { MatchingController } from './matching/matching.controller';
 import { MatchingModule } from './matching/matching.module';
+import { BookingModule } from './booking-service/booking.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { MatchingModule } from './matching/matching.module';
     }),
     MatchingModule,
     AuthModule,
+    BookingModule,
   ],
   controllers: [AppController, MatchingController],
   providers: [AppService],
